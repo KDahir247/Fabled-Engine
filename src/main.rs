b9575542084ec8cf686958ebec6850f9db1c2b90
Khalid <k.dahir243@hotmail.com>
2021-08-17 10:52:57 -0500

    Update main.rs
    re-enabled engine

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod core;
 mod test;
 
 fn main() {
-    superluminal_perf::set_current_thread_name("Main_Thread");
+    /*  superluminal_perf::set_current_thread_name("Main_Thread");
 
     superluminal_perf::begin_event("Creating Primitive");
 
@@ -10,7 +10,7 @@ fn main() {
     let plane_model: fabled_render::mesh::Model = plane.into();
 
     println!("a");
-    superluminal_perf::end_event();
-    /*    test::run();
-    core::State::run();*/
+    superluminal_perf::end_event();*/
+    test::run();
+    core::State::run();
 }
