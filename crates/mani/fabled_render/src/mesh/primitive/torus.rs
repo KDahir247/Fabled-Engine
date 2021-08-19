1f1d57b464c8fb112daa49482bc1c42bc6b1aac4
Khalid <k.dahir243@hotmail.com>
2021-08-19 10:18:17 -0500

    Finished Torus and tested.
    completed torus primitive and tested it in unity by passing the vertices, uv, normal, indices. The normal are calculated correctly because of how the directional light hit the torus and there is no apparent shadow artifact
    The test image can be found in
    Fabled Engine/sample/test/primitive/Torus_Primitive_Test.png

--- crates/mani/fabled_render/src/mesh/primitive/torus.rs
+++ crates/mani/fabled_render/src/mesh/primitive/torus.rs
@@ -8,6 +8,8 @@ use crate::mesh::{Mesh, Model, Vertex};
     //tessellation tubes,
 */
 
+#[derive(Debug, Clone, Copy, PartialEq)]
+#[repr(align(16))]
 pub struct Torus {
     /// The distance from the center of the tube to the center of the torus.
     major_radius: f32,
@@ -19,7 +21,7 @@ pub struct Torus {
 
 impl Default for Torus {
     fn default() -> Self {
-        Self::new(0.7, 0.3, 32, 12)
+        Self::new(1.0, 0.5, 32, 24)
     }
 }
 
@@ -48,33 +50,18 @@ impl From<Torus> for Model {
             tessellation_side,
         } = torus;
 
-        let total_vertices = tessellation_segment * tessellation_side;
+        let total_faces = tessellation_segment * tessellation_side;
+        let total_vertices = (tessellation_segment + 1) * (tessellation_side + 1);
 
-        let total_indices = (total_vertices << 1) * 3;
-
+        let total_indices = (total_faces << 1) * 3;
         let mut vertex_buffer = Vec::with_capacity(total_vertices);
         let mut indices = vec![0; total_indices];
 
         let inv_segment = 1.0 / tessellation_segment as f32;
         let inv_side = 1.0 / tessellation_side as f32;
 
-        for seg in 0..tessellation_segment {
-            // Find next (or first) segment offset
-            let next_segment = (seg + 1) % tessellation_segment;
-
-            let current_side_offset = seg * tessellation_side;
-            let next_side_offset = next_segment * tessellation_side;
-
-            for side in 0..tessellation_side {
-                // Find next (or first) vertex offset.
-                let next_side = (side + 1) % tessellation_side;
-
-                // Finding the 4 vertices that makes up the four corner of a quad.
-                let i1 = current_side_offset + side;
-                let i2 = current_side_offset + next_side;
-                let i3 = next_side_offset + next_side;
-                let i4 = next_side_offset + side;
-
+        for seg in 0..=tessellation_segment {
+            for side in 0..=tessellation_side {
                 // Calculate the UV texture coordinates.
                 let u_tex = seg as f32 * inv_segment;
                 let v_tex = side as f32 * inv_side;
@@ -83,36 +70,45 @@ impl From<Torus> for Model {
                 let (phi_sin, phi_cos) = (u_tex * std::f32::consts::TAU).sin_cos();
                 let (theta_sin, theta_cos) = (v_tex * std::f32::consts::TAU).sin_cos();
 
-                let x_pos = (major_radius + minor_radius * theta_cos) * phi_cos;
-                let y_pos = minor_radius * theta_sin;
-                let z_pos = (major_radius + minor_radius * theta_cos) * phi_sin;
+                let position = glam::Vec3A::new(
+                    (major_radius + minor_radius * theta_cos) * phi_cos,
+                    minor_radius * theta_sin,
+                    (major_radius + minor_radius * theta_cos) * phi_sin,
+                );
 
-                // Calculate the tangent with respect to the outer circle.
-                let tangents_outer = glam::Vec3A::new(-phi_sin, phi_cos, 0.0);
-
-                // Calculate the tangent with respect to the inner circle.
-                let tangents_inner =
-                    glam::Vec3A::new(phi_cos * -theta_sin, phi_sin * -theta_sin, theta_cos);
-
-                // Normal is the cross-product of the tangents.
-                let normal = tangents_outer.cross(tangents_inner).normalize();
+                // Calculate the Normal
+                let normal = glam::Vec3A::new(phi_cos * theta_cos, theta_sin, phi_sin * theta_cos)
+                    .normalize();
 
                 vertex_buffer.push(Vertex {
-                    position: [x_pos, y_pos, z_pos],
+                    position: position.to_array(),
                     tex_coord: [u_tex, v_tex],
                     normal: normal.to_array(),
                     tangent: [0.0; 4],
                     bi_tangent: [0.0; 4],
                 });
+            }
+        }
 
-                // Store the indices
-                indices[i1 * 6] = i1;
-                indices[i1 * 6 + 1] = i2;
-                indices[i1 * 6 + 2] = i3;
+        for seg in 0..tessellation_segment {
+            let current_side_offset = seg * tessellation_side;
+            let next_side_offset = (seg + 1) * tessellation_side;
 
-                indices[i1 * 6 + 3] = i3;
-                indices[i1 * 6 + 4] = i4;
-                indices[i1 * 6 + 5] = i1;
+            for side in 0..tessellation_side {
+                // Finding the 4 vertices that makes up the four corner of a quad.
+                let i1 = current_side_offset + side;
+                let i2 = current_side_offset + side + 1;
+                let i3 = next_side_offset + side + 1;
+                let i4 = next_side_offset + side + 2;
+
+                let face_indices = [i1 + seg, i2 + seg, i3 + seg, i2 + seg, i4 + seg, i3 + seg];
+
+                {
+                    //memcpy face_indices to 6 * i1 element slice of indices.
+                    let offset = i1 * 6;
+                    let (target_left, _) = indices[offset..].split_at_mut(6);
+                    target_left.copy_from_slice(&face_indices);
+                }
             }
         }
 
@@ -133,15 +129,20 @@ mod test {
 
     #[test]
     fn test() {
-        let torus = Torus::default();
+        let torus = Torus {
+            major_radius: 1.0,
+            minor_radius: 0.5,
+            tessellation_segment: 32,
+            tessellation_side: 24,
+        };
         let torus_model: Model = torus.into();
 
         for vertex in &torus_model.meshes[0].vertices {
             println!(
                 "new Vector3({}f, {}f, {}f),",
-                vertex.position[0], vertex.position[1], vertex.position[2]
+                vertex.normal[0], vertex.normal[1], vertex.normal[2]
             );
         }
-        //println!("{:?}", torus_model.meshes[0].indices);
+        println!("{:?}", torus_model.meshes[0].indices);
     }
 }
