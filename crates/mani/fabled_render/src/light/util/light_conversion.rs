8bafe621683c7981f9a59e192a78d61acd3b6d72
Khalid <k.dahir243@hotmail.com>
2021-09-12 14:07:25 -0500

    continue working on light conversion script
    Implemented point light calculating lumious power from luminous intensity and vis versa. Going to work on spot light tomorrow morning and continue researching on lighting.

--- crates/mani/fabled_render/src/light/util/light_conversion.rs
+++ crates/mani/fabled_render/src/light/util/light_conversion.rs
@@ -14,6 +14,7 @@ pub fn lux_to_candela(lux: f32, distance: f32) -> f32 {
     lux * distance * distance
 }
 
+// For uniform, isotropic light source.
 // Candela to lumens where apex angle is the full angle in degree of the the
 // light apex.
 // luminance intensity in candela (cd) to luminance flux in lumen (lm)
@@ -23,6 +24,7 @@ pub fn candela_to_lumen(candela: f32, apex_angle: f32) -> f32 {
     candela * (std::f32::consts::TAU * (1.0 - (target_angle / 2.0).cos()))
 }
 
+// For uniform, isotropic light source,
 // Opposite operation of candela to lumen
 // Luminance flux in lumen (lm) to luminance intensity in candela (cd)
 // cd = lm / ( 2π(1 - cos(º/2)) )
@@ -71,11 +73,50 @@ pub fn ev_to_luminance(ev: f32, iso: ISOSpeed, calibration_constant: Option<f32>
     (2.0f32.powf(ev) * calibration_constant) / iso.arithmetic_speed
 }
 
+// Opposite operation of ev to luminance (nit)
+// EV = log2((luminance * S) / reflected-light meter calibration constant)
+pub fn luminance_to_ev(luminance: f32, iso: ISOSpeed, calibration_constant: Option<f32>) -> f32 {
+    let calibration_constant = calibration_constant.unwrap_or(CALIBRATION_CONSTANT_PRIMARY);
+
+    f32::log2((luminance * iso.arithmetic_speed) / calibration_constant)
+}
+
+
+// Calculate point light's luminance power (luminance flux) from the luminance
+// intensity
+// φ = 4π I
+// which is equivalent to φ = 2τ I
+// where τ = tau == 2π
+// where φ = luminous power and I = luminous intensity
+pub fn point_light_candela_to_lumen(candela: f32) -> f32 {
+    2.0 * std::f32::consts::TAU * candela
+}
+
+// Opposite operation of point light luminance power (luminance flux) to the
+// luminance Calculate point light's luminance intensity from luminance power
+// (luminance flux)
+// 	I = 4π / φ
+// which is equivalent to I = φ / 2τ
+// where τ = tau == 2π
+pub fn point_light_lumen_to_candela(lumen: f32) -> f32 {
+    lumen / (2.0 * std::f32::consts::TAU)
+}
+
+pub fn spot_light_candela_to_lumen(candela: f32) -> f32 {
+    unimplemented!()
+}
+
+pub fn spot_light_lumen_to_candela(lumen: f32) -> f32 {
+    unimplemented!()
+}
 
 /// result have been retrieve from [light calculation](https://www.rapidtables.com/calc/light/index.html)
 #[cfg(test)]
 mod unit_conversion_tests {
-    use crate::light::{candela_to_lumen, candela_to_lux, lumen_to_candela, lux_to_candela};
+    use crate::light::{
+        candela_to_lumen, candela_to_lux, ev_to_luminance, lumen_to_candela, luminance_to_ev,
+        lux_to_candela, point_light_candela_to_lumen, point_light_lumen_to_candela, ISOSpeed,
+    };
 
     #[test]
     fn candela_lux_test() {
@@ -113,5 +154,30 @@ mod unit_conversion_tests {
         let revert_in_question = lumen_to_candela(in_question, apex_angle);
 
         assert!(revert_in_question.eq(&luminance_intensity));
+    }
+
+    #[test]
+    fn ev_luminance() {
+        // See EV100 to Luminance Chart above
+        let ev = 7.0;
+        let iso_arithmetic = ISOSpeed::default();
+        let luminance = ev_to_luminance(ev, iso_arithmetic, None);
+        assert!(luminance.eq(&16.0));
+
+        let in_question_ev = luminance_to_ev(luminance, iso_arithmetic, None);
+        assert!(in_question_ev.eq(&ev));
+    }
+
+
+    #[test]
+    fn point_light_candela_lumen() {
+        let candela = 150.0;
+
+        let lumen = point_light_candela_to_lumen(candela);
+        println!("{}", lumen);
+
+
+        let in_question_candela = point_light_lumen_to_candela(lumen);
+        println!("{}", in_question_candela);
     }
 }
