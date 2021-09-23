f620084cb67afb4ce7bdaf156f4c2ed391f8733f
Khalid <k.dahir243@hotmail.com>
2021-09-23 10:45:27 -0500

    audio init test
    playing with audio. Currently there is only on test called it works in the audio module that will play the audio

--- crates/mani/fabled_audio/src/lib.rs
+++ crates/mani/fabled_audio/src/lib.rs
@@ -1,7 +1,21 @@
+// Audio Death Is Just Another Path.mp3 Made by Otto Halmén
+// https://opengameart.org/content/death-is-just-another-path
+
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        let (_stream, handle) = rodio::OutputStream::try_default().unwrap();
+        let sink = rodio::Sink::try_new(&handle).unwrap();
+
+
+        let mut a = String::new();
+        let cargo_dir = env!("CARGO_MANIFEST_DIR");
+        a.push_str(cargo_dir);
+        a.push_str("/Death Is Just Another Path.mp3");
+        let file = std::fs::File::open(a.as_str()).unwrap();
+
+        sink.append(rodio::Decoder::new(std::io::BufReader::new(file)).unwrap());
+        sink.sleep_until_end();
     }
 }
