d9a4ae61cb1ebf319966599646e325325f4b4361
Khalid <k.dahir243@hotmail.com>
2021-10-04 17:34:46 -0500

    tested raw_clip and raw_ambisonic_clip script
    tested  raw_clip and raw_ambisonic_clip  script and re-looked at the script. It looks simple and good to continue to the next script. I have added test to show the design of the script and make sure everything is running correctly currently and in the future when making changes.

--- crates/mani/fabled_audio/src/container/raw_clip.rs
+++ crates/mani/fabled_audio/src/container/raw_clip.rs
@@ -1,5 +1,3 @@
-// todo Check if I really need this.
-
 pub struct RawClip<T>
 where
     T: rodio::Source,
@@ -59,5 +57,76 @@ where
 
     pub fn get(self) -> T {
         self.data
+    }
+}
+
+
+#[cfg(test)]
+mod raw_clip_test {
+    use crate::{AudioClip, Standard};
+    use ambisonic::rodio::Source;
+    use std::io::Read;
+
+    #[test]
+    fn get_inner() {
+        let path = &[env!("CARGO_MANIFEST_DIR"), "/src/audio/epic1.mp3"].join("");
+
+        let mut file = std::fs::File::open(path).unwrap();
+        let mut audio_buffer = vec![0; file.metadata().unwrap().len() as usize];
+        file.read_exact(&mut audio_buffer).unwrap();
+
+
+        let clip: AudioClip<f32> = AudioClip::from_file(audio_buffer, true);
+
+        let previous_channel = clip.channels();
+        let previous_sample_rate = clip.sample_rate();
+        let previous_total_duration = clip.total_duration();
+        let previous_curr_len = clip.current_frame_len();
+
+        let raw_clip = Standard::from(clip);
+
+        let _ref_inner = raw_clip.as_ref();
+
+        let inner = raw_clip.get();
+
+        assert!(previous_channel.eq(&inner.channels()));
+        assert!(previous_sample_rate.eq(&inner.sample_rate()));
+        assert!(previous_total_duration.eq(&inner.total_duration()));
+        assert!(previous_curr_len.eq(&inner.current_frame_len()));
+    }
+}
+
+
+#[cfg(test)]
+mod raw_ambisonic_clip_test {
+    use crate::{Ambisonic, AudioClip};
+    use ambisonic::rodio::Source;
+    use std::io::Read;
+
+    #[test]
+    fn get_inner() {
+        let path = &[env!("CARGO_MANIFEST_DIR"), "/src/audio/epic1.mp3"].join("");
+
+        let mut file = std::fs::File::open(path).unwrap();
+        let mut audio_buffer = vec![0; file.metadata().unwrap().len() as usize];
+        file.read_exact(&mut audio_buffer).unwrap();
+
+        let clip: AudioClip<f32> = AudioClip::from_file(audio_buffer, true);
+
+        let previous_channel = clip.channels();
+        let previous_sample_rate = clip.sample_rate();
+        let previous_total_duration = clip.total_duration();
+        let previous_curr_len = clip.current_frame_len();
+
+        let raw_ambisonic = Ambisonic::from(clip);
+
+        let _ref_inner = raw_ambisonic.as_ref();
+
+        let inner = raw_ambisonic.get();
+
+        assert!(inner.channels().eq(&previous_channel));
+        assert!(inner.sample_rate().eq(&previous_sample_rate));
+        assert!(inner.total_duration().eq(&previous_total_duration));
+        assert!(inner.current_frame_len().eq(&previous_curr_len));
     }
 }
