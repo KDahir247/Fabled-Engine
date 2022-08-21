18dd474cf9050fad0885dcadc553d44b609b3314
Khalid <k.dahir243@hotmail.com>
2022-08-20 19:32:55 -0500

    cleaning code source
    currently going through code source and improving it. Some script file has been commented all out, due to breaking from changes. I will re-work on them when I am happy with the overall refactoring of the code source then go through them one by one.

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,6 @@
-use fabled_render::mesh::{Cone, Cube, Mesh};
-
 mod core;
 mod test;
 
 fn main() {
-    superluminal_perf::begin_event("start primitive");
-
-    let a = Cone::default();
-
-    for c in 0..1000 {
-        superluminal_perf::begin_event("Primitive_call");
-
-        let b: Mesh = Mesh::from(a);
-        superluminal_perf::end_event();
-
-        println!("{:?}", b.vertices.len());
-    }
-
-    superluminal_perf::end_event();
-
-    // core::State::run();
+    core::State::run();
 }
