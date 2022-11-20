161df970039691eb7615c25b20be972da234baa3
Khalid <k.dahir243@hotmail.com>
2022-11-20 14:44:48 -0600

    going to work on the light section now.
    I am going to work on the lighting section now. The camera section is missing the following;
    culling mask, background color, priority, aspect ratio mode, set letter box, and set pillar box for aspect ratio.

--- crates/mani/fabled_render/src/camera/container/mod.rs
+++ crates/mani/fabled_render/src/camera/container/mod.rs
@@ -1,11 +1,9 @@
-mod aspect_ratio_mode;
 mod camera_format;
 mod fish_eye_len;
 mod fov_scaling;
 mod measurement_type;
 mod unit_type;
 
-pub use aspect_ratio_mode::*;
 pub use camera_format::*;
 pub use fish_eye_len::*;
 pub use fov_scaling::*;
